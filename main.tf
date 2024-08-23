99a516fcf7981bbf14432fbc385eff42fd912a77
Matteo Baiguini <baiguini.matteo@gmail.com>
2024-08-23 17:43:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,24 +8,24 @@ provider "google" {
     project   = var.PROJECT_ID
 }
 
-resource "google_service_account" "terraform_operator_sample" {
-  account_id     = "terraform-operator-sample"
+resource "google_service_account" "terraform_operator_editor" {
+  account_id     = "tf-editor"
   display_name   = "Service Account created by Terraform K8s Operator"
 }
 
-resource "google_project_iam_member" "terraform_operator_sample_binding" {
+resource "google_project_iam_member" "terraform_operator_editor_binding" {
   project   = var.PROJECT_ID
   role      = "roles/editor"
-  member    = "serviceAccount:${google_service_account.terraform_operator_sample.email}"
+  member    = "serviceAccount:${google_service_account.terraform_operator_editor.email}"
 }
 
-resource "google_service_account" "terraform_operator_another_sample" {
-  account_id     = "terraform-operator-another-sample"
-  display_name   = "Another Service Account created by Terraform K8s Operator"
+resource "google_service_account" "terraform_operator_viewer" {
+  account_id     = "tf-viewer"
+  display_name   = "Service Account created by Terraform K8s Operator"
 }
 
-resource "google_project_iam_member" "terraform_operator_another_sample_binding" {
+resource "google_project_iam_member" "terraform_operator_viewer_binding" {
   project   = var.PROJECT_ID
   role      = "roles/viewer"
-  member    = "serviceAccount:${google_service_account.terraform_operator_another_sample.email}"
+  member    = "serviceAccount:${google_service_account.terraform_operator_viewer.email}"
 }
