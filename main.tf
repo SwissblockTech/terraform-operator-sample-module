2208d9e6fcf2a8ea764e5d53558d8718581faccc
Matteo Baiguini <baiguini.matteo@gmail.com>
2024-08-23 17:42:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 variable "PROJECT_ID" {
   description = "GCP Project ID"
   type        = string
+  # default     = "TypeHere"
 }
 
 provider "google" {
@@ -16,4 +17,15 @@ resource "google_project_iam_member" "terraform_operator_sample_binding" {
   project   = var.PROJECT_ID
   role      = "roles/editor"
   member    = "serviceAccount:${google_service_account.terraform_operator_sample.email}"
+}
+
+resource "google_service_account" "terraform_operator_another_sample" {
+  account_id     = "terraform-operator-another-sample"
+  display_name   = "Another Service Account created by Terraform K8s Operator"
+}
+
+resource "google_project_iam_member" "terraform_operator_another_sample_binding" {
+  project   = var.PROJECT_ID
+  role      = "roles/viewer"
+  member    = "serviceAccount:${google_service_account.terraform_operator_another_sample.email}"
 }
